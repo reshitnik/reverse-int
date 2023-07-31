2e1d3a5e59f370e154d8a7ab4f0959ac12e5b55d
reshitnik <reshitnik@gmail.com>
2023-07-31 18:45:26 +0300

    complete task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 module.exports = function reverse (n) {
-  
-}
+    n = Math.abs(n); 
+    let reversNumber='';
+    while (n>0){
+        reversNumber += n % 10;
+        n = Math.floor(n / 10);
+    };
+    return Number(reversNumber)
+};
